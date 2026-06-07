fa8bc31d09bd6b72c1bd2fda93dbbdde8a24099a
Sean Gould <sean.gould@fluid-code.com>
2026-06-07 10:25:31 -0600

    renamed file from janr_logger.py to loggger.py

---
+++ ansible/roles/blocklist/src/core/logger.py
@@ -0,0 +1,25 @@
+# core/logger.py
+
+from datetime import datetime
+from pathlib import Path
+
+
+class Logger:
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    DEBUG = "DEBUG"
+
+    def __init__(self, file="/run/janr/log/blocklist.log"):
+
+        self.file = Path(file)
+        self.file.parent.mkdir(parents=True, exist_ok=True)
+
+    def log(self, msg, lvl=INFO):
+        ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        line = f"[{ts}] [{lvl}] {msg}\n"
+        with open(self.file, "a") as f:
+            f.write(line)
+
+
+logger = Logger()
